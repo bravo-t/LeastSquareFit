8d71b3fa59a2f4fe679991cd974eddc83cd0678c
13inback <13inback@gmail.com>
2024-11-22 08:48:33 +0000

    update

--- LeastSquareFit.cpp
+++ LeastSquareFit.cpp
@@ -1,1 +1,49 @@
+#include <cassert>
 #include "LeastSquareFit.h"
+
+
+class Matrix {
+  public:
+    Matrix(size_t rows, size_t cols)
+    : _rows(rows), _cols(cols)
+    {
+      _data.assign(_rows * _cols, 0);
+    }
+
+    ~Matrix();
+
+    size_t width() const { return _cols; }
+    size_t height() const { return _rows; }
+
+    double operator()(size_t row, size_t col) const
+    {
+      return _data[row * _cols + col];
+    }
+    
+    double& operator()(size_t row, size_t col) 
+    {
+      return _data[row * _cols + col];
+    }
+
+    Matrix operator*(const Matrix& other) const;
+
+    std::vector<double> data() const { return _data; }
+  private:
+    size_t _rows = 0;
+    size_t _cols = 0;
+    std::vector<double> _data;
+};
+
+Matrix Matrix::operator*(const Matrix& other) const
+{
+  assert(_cols == other._rows);
+  Matrix result(_rows, other._cols);
+  for (size_t i=0; i<result.height(); ++i) {
+    for (size_t j=0; j<result.width(); ++j) {
+      for (size_t k=0; k<_cols; ++k) {
+        result(i, j) += this->operator()(i, k) * other(k, j);
+      }
+    }
+  }
+  return result;
+}
--- LeastSquareFit.h
+++ LeastSquareFit.h
@@ -1,12 +1,36 @@
 #ifndef _LSFIT_H_
 #define _LSFIT_H_
 
+#include <vector>
+#include <functional>
+
 class LeastSquareFit {
   public:
     LeastSquareFit();
     ~LeastSquareFit();
+
+    using FitFunction = std::function<double(const std::vector<double>& params, const std::vector<double>& x)>;
+    using FitVecFunction = std::function<std::vector<double>(const std::vector<double>& params, const std::vector<double>& x)>;
+    void setFitFunction(const FitFunction& func);
+    void setFitFunction(const FitVecFunction& func);
+    void addDerivativeFunction(const FitFunction& devFunc);
+
+    void run();
+    std::vector<double> parameters() const { return _params; }
+    double standardError() const { return _error; }
+
   private:
+    bool useNumericalDerivative() const { return _derivatives.empty(); }
+    double numericalDerivative() const;
+    double derivative() const;
 
+  private:
+    FitVecFunction                   _fitFunction;
+    std::vector<FitFunction>         _derivatives;
+    std::vector<double>              _params;
+    double                           _error;
+    std::vector<std::vector<double>> _obsX;
+    std::vector<std::vector<double>> _obsY;
 };
-
+        
 #endif
